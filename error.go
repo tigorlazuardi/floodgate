625c02e36ecd00a7c7dabbc42ece978336611f9e
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-08-04 11:23:36 +0700

    added error struct object

---
+++ error.go
@@ -0,0 +1,40 @@
+package floodgate
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type GateError struct {
+	Name      string
+	OriginErr error
+	Err       string
+}
+
+func newGateError(name string, err error) GateError {
+	return GateError{
+		Name:      name,
+		OriginErr: err,
+		Err:       err.Error(),
+	}
+}
+
+func (ge GateError) Error() string {
+	return ge.Err
+}
+
+func (ge GateError) String() string {
+	return fmt.Sprintf("service '%s' return error: '%s'", ge.Name, ge.Err)
+}
+
+func (ge GateError) MarshalJSON() ([]byte, error) {
+	m := make(map[string]interface{})
+	m["name"] = ge.Name
+	if val, _ := json.Marshal(ge.OriginErr); string(val) == "{}" || string(val) == "" {
+		m["cause"] = ge.Err
+	} else {
+		m["cause"] = string(val)
+	}
+	m["error"] = ge.Err
+	return json.Marshal(m)
+}
--- register.go
+++ register.go
@@ -1,8 +1,29 @@
 package floodgate
 
-func (gate *Gate) RegisterService(name string, f CheckerFunc) *Gate {
+import (
+	"context"
+	"errors"
+)
 
-	return gate
+func (gate *Gate) RegisterService(name string, f CheckerFunc) *Gate {
+	safe := func(ctx context.Context) (err error) {
+		defer func() {
+			if rec := recover(); rec != nil {
+				if e, ok := rec.(error); ok {
+					err = newGateError(name, e)
+					return
+				}
+				if e, ok := rec.(string); ok {
+					err = newGateError(name, errors.New(e))
+					return
+				}
+				err = newGateError(name, errors.New("panic caught on health checking service: "+name))
+			}
+		}()
+		err = f(ctx)
+		return
+	}
+	return gate.RegisterHandler(name, &service{check: safe})
 }
 
 func (gate *Gate) RegisterHandler(name string, c Checker) *Gate {
